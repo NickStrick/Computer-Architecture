64c2f4feb9a5e69ddd90e08a519514af63efede1
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-14 17:31:26 -0500

    steps 0-8 of ls8 project compelte

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,42 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
+        program = []
+        f = open(sys.argv[1], "r")
+        for line in f:
+            li = line.strip()
+            if not li.startswith("#"):
+                if li != '':
+                    curr = line.rstrip()
+                    program.append(int(f'0b{curr.split()[0]}', 2))
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        self.ram = [0] * (len(program)+2)
+        print(program, len(program), self.ram)
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
+        print(self.ram)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.pc += 3
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +49,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +63,40 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 1
+        LDI = 130
+        PRN = 71
+        MUL = 162
+        running = True
+
+        while running:
+            # Do stuff
+            op = self.ram[self.pc]
+            reg_a = self.ram[self.pc + 1]
+            reg_b = self.ram[self.pc+2]
+            if op == HLT:
+                sys.exit(1)
+                running = False
+            elif op == LDI:
+                num = reg_b  # Get the num from 1st arg
+                # Get the reg index from 2nd arg
+                reg = reg_a
+                self.reg[reg] = num  # Store the num in the right reg
+                self.pc += 3
+            elif op == PRN:
+                # Get the reg index from 1st arg
+                reg = reg_a
+                print(self.reg[reg])  # Print contents of that reg
+                self.pc += 2
+            elif op == MUL:
+                self.reg[reg_a] *= self.reg[reg_b]
+                self.pc += 3
+            else:
+                # self.alu(op, self.ram[self.pc + 1], self.ram[self.pc+2])
+                print('no valid Command')
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, value, mar):
+        self.ram[mar] = value
