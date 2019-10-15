bfeb00bc7bb69d547ac467b6ae8138113a89c73d
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-10-15 14:48:30 -0500

    optimized run loop to O(1)

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,6 +9,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 8
+        self.ram = [0] * 256
         self.pc = 0
         self.sp = 0
         self.stack = []
@@ -23,15 +24,13 @@ class CPU:
                 if li != '':
                     curr = line.rstrip()
                     program.append(int(f'0b{curr.split()[0]}', 2))
+        f.close()
 
         address = 0
-        self.ram = [0] * (len(program)+2)
-        # print(program, len(program), self.ram)
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-        print(self.ram)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -65,63 +64,74 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        HLT = 1
-        LDI = 130
-        PRN = 71
-        MUL = 162
-        PUSH = 69
-        POP = 70
-        CALL = 80
-        RET = 17
-        ADD = 160
         running = True
 
+        def HLT(self, op, reg_a, reg_b):
+            sys.exit(1)
+            running = False
+
+        def LDI(self, op, reg_a, reg_b):
+            num = reg_b  # Get the num from 1st arg
+            # Get the reg index from 2nd arg
+            reg = reg_a
+            self.reg[reg] = num  # Store the num in the right reg
+            self.pc += 3
+
+        def PRN(self, op, reg_a, reg_b):
+            # Get the reg index from 1st arg
+            reg = reg_a
+            print(self.reg[reg])  # Print contents of that reg
+            self.pc += 2
+
+        def MUL(self, op, reg_a, reg_b):
+            self.reg[reg_a] *= self.reg[reg_b]
+            self.pc += 3
+
+        def ADD(self, op, reg_a, reg_b):
+            self.reg[reg_a] += self.reg[reg_b]
+            self.pc += 3
+
+        def PUSH(self, op, reg_a, reg_b):
+            self.stack.append(self.reg[reg_a])
+            self.pc += 2
+
+        def POP(self, op, reg_a, reg_b):
+            value = self.stack[-1]
+            del self.stack[-1]
+            self.reg[reg_a] = value
+            self.pc += 2
+
+        def CALL(self, op, reg_a, reg_b):
+            self.stack.append(self.pc + 2)
+            self.pc = self.reg[reg_a]
+
+        def RET(self, op, reg_a, reg_b):
+            value = self.stack[-1]
+            del self.stack[-1]
+            self.pc = value
+
+        def exception(self, op, reg_a, reg_b):
+            print('no valid Command')
+            self.pc += 1
+
+        instructions = {
+            1: HLT,
+            130: LDI,
+            71: PRN,
+            162: MUL,
+            69: PUSH,
+            70: POP,
+            80: CALL,
+            17: RET,
+            160: ADD
+        }
+
         while running:
-            # Do stuff
-            # print(self.pc)
             op = self.ram[self.pc]
             reg_a = self.ram[self.pc + 1]
             reg_b = self.ram[self.pc+2]
-            if op == HLT:
-                sys.exit(1)
-                running = False
-            elif op == LDI:
-                num = reg_b  # Get the num from 1st arg
-                # Get the reg index from 2nd arg
-                reg = reg_a
-                self.reg[reg] = num  # Store the num in the right reg
-                self.pc += 3
-            elif op == PRN:
-                # Get the reg index from 1st arg
-                reg = reg_a
-                print(self.reg[reg])  # Print contents of that reg
-                self.pc += 2
-            elif op == MUL:
-                self.reg[reg_a] *= self.reg[reg_b]
-                self.pc += 3
-            elif op == ADD:
-                self.reg[reg_a] += self.reg[reg_b]
-                self.pc += 3
-            elif op == PUSH:
-                self.stack.append(self.reg[reg_a])
-                self.pc += 2
-            elif op == POP:
-                value = self.stack[-1]
-                del self.stack[-1]
-                self.reg[reg_a] = value
-                self.pc += 2
-            elif op == CALL:
-                self.stack.append(self.pc + 2)
-                self.pc = self.reg[reg_a]
 
-            elif op == RET:
-                value = self.stack[-1]
-                del self.stack[-1]
-                self.pc = value
-
-            else:
-                print('no valid Command')
-                self.pc += 1
+            instructions[op](self, op, reg_a, reg_b)
 
     def ram_read(self, mar):
         return self.ram[mar]
